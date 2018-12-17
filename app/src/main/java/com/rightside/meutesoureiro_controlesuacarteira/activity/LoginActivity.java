ed3111f704b8af529aa5430cc5f224c19db751a7
Matheus <matheusldasilva20088@gmail.com>
2018-12-17 20:07:51 -0200

    Slide introducao tela de login e cadastro

---
+++ app/src/main/java/com/rightside/meutesoureiro_controlesuacarteira/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.rightside.meutesoureiro_controlesuacarteira.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rightside.meutesoureiro_controlesuacarteira.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
